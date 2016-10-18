84802a825dd2cd27deb68fd7ff53b209abfb3d71
Edward Billington <billin22@coventry.ac.uk>
2016-10-18 14:43:53 +0100

    Got a working drawn board design done

--- TestGUIs/pygame/pygameDrawingRandom.py
+++ TestGUIs/pygame/pygameDrawingRandom.py
@@ -16,8 +16,14 @@ DISPLAYSURF.fill(BLACK)
 
 pygame.draw.line(DISPLAYSURF, WHITE, (1, 0), (1, 600), 3) #left
 pygame.draw.line(DISPLAYSURF, WHITE, (0, 1), (600, 1), 3) #top
-pygame.draw.line(DISPLAYSURF, WHITE, (598, 0), (598, 598), 3) #right
+pygame.draw.line(DISPLAYSURF, WHITE, (598, 0), (598, 600), 3) #right
 pygame.draw.line(DISPLAYSURF, WHITE, (0, 598), (598, 598), 3) #bottom
+
+pygame.draw.line(DISPLAYSURF, WHITE, (0, 197), (600, 197), 3) # H1
+pygame.draw.line(DISPLAYSURF, WHITE, (0, 397), (600, 397), 3) # H2
+
+pygame.draw.line(DISPLAYSURF, WHITE, (197, 0), (197, 600), 3) #V1
+pygame.draw.line(DISPLAYSURF, WHITE, (397, 0), (397, 600), 3) #V2
 
 # run the game loop
 while True:
