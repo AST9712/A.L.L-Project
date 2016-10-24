3ec3eb5a9f398aa72c8ea3fe3b4f5ae9f35d2716
edwardbillin22 <billin22@uni.coventry.ac.uk>
2016-10-24 23:21:13 +0100

    Error detection implemented and now clicking will draw an 'X' in the appropriate square

--- TIC-TAC-TOE/main.py
+++ TIC-TAC-TOE/main.py
@@ -7,9 +7,7 @@ pygame.init()
 
 WIN_H = 600
 WIN_W = 700
-
-a, b = 3, 3
-boardMatrix = [[0 for x in range(a)] for y in range(b)]
+CURRENT_PLYR = 'X'
 
 BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
@@ -18,11 +16,17 @@ WINDOW = pygame.display.set_mode((WIN_H, WIN_W), 0, 32)
 CHAR_FONT = pygame.font.SysFont("monospace", 200)
 TEXT_FONT = pygame.font.SysFont("monospace", 12)
 
+class board: # make this its own file later
+    
+    boardMatrix = [[0 for x in range(3)] for y in range(3)]
+
+
 def makeWindow():    
     pygame.display.set_caption('TIC-TAC-TOE')
     drawBoard()
 
 def drawBoard():
+    
     WINDOW.fill(BLACK)
     
     pygame.draw.line(WINDOW, WHITE, (1, 0), (1, 699), 3)  # left
@@ -39,20 +43,64 @@ def drawBoard():
 
     gameStart()
 
-def drawX(COORD):
-    global boardMatrix
-    drawXCHAR = CHAR_FONT.render("X", True, WHITE)
-    x, y = COORD
-    if x < 200 and y < 200 and boardMatrix[0][0] == 0:
-        boardMatrix[0][0] = 1
-        WINDOW.blit(drawXCHAR, (40, -10))
+def checkValidClick():
+    
+    x, y = pygame.mouse.get_pos()
+    
+    if x < 200 and y < 200:       
+        if board.boardMatrix[0][0] == 0:            
+            drawX(40, -10)
+            board.boardMatrix[0][0] = 1        
+        
     elif x < 400 and y < 200:
-        WINDOW.blit(drawXCHAR, (240, -10))
+        if board.boardMatrix[1][0] == 0: 
+            drawX(240, -10)
+            board.boardMatrix[1][0] = 1
+        
     elif x < 600 and y < 200:
-        WINDOW.blit(drawXCHAR, (440, -10))
+        if board.boardMatrix[2][0] == 0: 
+            drawX(440, -10)
+            board.boardMatrix[2][0] = 1
+
+    elif x < 200 and y < 400:
+        if board.boardMatrix[0][1] == 0: 
+            drawX(40, 190)
+            board.boardMatrix[0][1] = 1
+
+    elif x < 400 and y < 400:
+        if board.boardMatrix[1][1] == 0: 
+            drawX(240, 190)
+            board.boardMatrix[1][1] = 1
+
+    elif x < 600 and y < 400:
+        if board.boardMatrix[2][1] == 0: 
+            drawX(440, 190)
+            board.boardMatrix[2][1] = 1
+
+    elif x < 200 and y < 600:
+        if board.boardMatrix[0][2] == 0: 
+            drawX(40, 390)
+            board.boardMatrix[0][2] = 1
+
+    elif x < 400 and y < 600:
+        if board.boardMatrix[1][2] == 0: 
+            drawX(240, 390)
+            board.boardMatrix[1][2] = 1
+
+    elif x < 600 and y < 600:
+        if board.boardMatrix[2][2] == 0: 
+            drawX(440, 390)
+            board.boardMatrix[2][2] = 1
+        
+    
+
+def drawX(a, b):
+    
+    drawXCHAR = CHAR_FONT.render("X", True, WHITE) 
+    WINDOW.blit(drawXCHAR, (a, b))       
 
 def gameStart():
-       
+    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -60,7 +108,7 @@ def gameStart():
                 sys.exit()
                 
             if event.type == pygame.MOUSEBUTTONUP:
-                drawX(pygame.mouse.get_pos())
+                checkValidClick()
                     
         pygame.display.update()
 
