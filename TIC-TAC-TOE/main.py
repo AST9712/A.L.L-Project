2ec8f4f8d837991c21c53f614470793cd30d5875
Edward Billington <billin22@coventry.ac.uk>
2016-10-24 10:49:53 +0100

    Clicking in the right square now draws X's and checks for already clicked squares

--- TIC-TAC-TOE/main.py
+++ TIC-TAC-TOE/main.py
@@ -8,6 +8,9 @@ pygame.init()
 WIN_H = 600
 WIN_W = 700
 
+a, b = 3, 3
+boardMatrix = [[0 for x in range(a)] for y in range(b)]
+
 BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
 
@@ -33,14 +36,23 @@ def drawBoard():
 
     pygame.draw.line(WINDOW, WHITE, (197, 0), (197, 599), 3)  # V1
     pygame.draw.line(WINDOW, WHITE, (397, 0), (397, 599), 3)  # V2
-   
+
     gameStart()
 
 def drawX(COORD):
-    X = CHAR_FONT.render("X", True, WHITE)
-    WINDOW.blit(X, COORD)
+    global boardMatrix
+    drawXCHAR = CHAR_FONT.render("X", True, WHITE)
+    x, y = COORD
+    if x < 200 and y < 200 and boardMatrix[0][0] == 0:
+        boardMatrix[0][0] = 1
+        WINDOW.blit(drawXCHAR, (40, -10))
+    elif x < 400 and y < 200:
+        WINDOW.blit(drawXCHAR, (240, -10))
+    elif x < 600 and y < 200:
+        WINDOW.blit(drawXCHAR, (440, -10))
 
 def gameStart():
+       
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
