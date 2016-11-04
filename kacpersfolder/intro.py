cb53328196ddcac0c9dc624c49bebef4ad24b183
Kacper Madry <madryk@uni.coventry.ac.uk>
2016-11-04 10:41:32 +0000

    Centered the start menu text

--- kacpersfolder/intro.py
+++ kacpersfolder/intro.py
@@ -1,7 +1,13 @@
 """
 Intro for the TIC-TAC-TOE game for our ALL Project 
 """
-import pygame
+import pygame, sys
+from pygame.locals import *
+
+#variables
+screenWidth = 480
+screenHeight = 380
+centerW = (screenWidth/2) - 80
 
 class Option:
 
@@ -18,9 +24,9 @@ class Option:
         screen.blit(self.rend, self.rect)
         
     def set_rend(self):
-        self.rend = menu_font.render(self.text, True, self.get_color())
+        self.rend = menu_font.render(self.text, 1, self.textcol())
         
-    def get_color(self):
+    def textcol(self):
         if self.hovered:
             return (255, 255, 255)
         else:
@@ -32,10 +38,11 @@ class Option:
         self.rect.topleft = self.pos
 
 pygame.init()
-screen = pygame.display.set_mode((480, 320))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption('TIC-TAC-TOE')
 menu_font = pygame.font.Font(None, 40)
-options = [Option("NEW GAME", (140, 105)), Option("LOAD GAME", (135, 155)),
-           Option("OPTIONS", (145, 205))]
+options = [Option("Single-Player", (centerW, 110)), Option("Multi-Player", (centerW, 160)), Option("VS Computer", (centerW, 210))]
+
 while True:
     pygame.event.pump()
     screen.fill((0, 0, 0))
@@ -45,4 +52,8 @@ while True:
         else:
             option.hovered = False
         option.draw()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()   
     pygame.display.update()
